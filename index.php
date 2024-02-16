23ad2d2a00afc35bad122bdae7fe9ed3f07c15bb
geekycoder2506 <160204953+geekycoder2506@users.noreply.github.com>
2024-02-16 16:01:53 +0400

    Update index.php

--- index.php
+++ index.php
@@ -17,7 +17,7 @@ $result = $conn->query($sql);
 if ($result->num_rows > 0) {
   // output data of each row
   while($row = $result->fetch_assoc()) {
-    echo " - Name: " . $row["firstname"]. " "<br>";
+    echo " - Name: " . $row["name"]. " "<br>";
   }
 } else {
   echo "0 results";
